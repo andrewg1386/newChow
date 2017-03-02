29225d8309029c6f9848ece7fa91b43bca336b52
Andrew Gonzalez <coolguy1386@yahoo.com>
2017-03-01 17:18:03 -0800

    Log Reg working, map locating and some other stuff

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,8 +8,7 @@ class ClientsController < ApplicationController
     end
     :back
   end
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   def new
   @client = client.create( name: params[:name], description: params[:description])
   #this redirect only applies for when the client was successfully created! You'll have to modify this  code with an if statement to RENDER the new view IF there are errors ELSE REDIRECT to the clients view if there weren't errors.
@@ -22,14 +21,8 @@ def show
   @clients = Client.search(params[:city])
 end
 
-private
-  def client_params
-    params.require(:client).permit(:name, :email, :password)
-  end
 
-=======
-=======
->>>>>>> a1a153c7bf129cc7cadf8acd0b3c6fe645bc1bef
+
 
   def log_reg
   end
@@ -42,17 +35,12 @@ private
 
     if client.save
       session[:client_id] = client.id
-    else
-      flash[:errors]=clients.errors.full_messages
       redirect_to :back
     end
   end
   private
   def client_params
-    params.require(:client).permit(:name,:email,:password,:password_confirmation)
+    params.require(:client).permit(:name,:email,:password,:password_confirmation,:street,:city,:state,:zip)
   end
-<<<<<<< HEAD
->>>>>>> a1a153c7bf129cc7cadf8acd0b3c6fe645bc1bef
-=======
->>>>>>> a1a153c7bf129cc7cadf8acd0b3c6fe645bc1bef
+
 end
